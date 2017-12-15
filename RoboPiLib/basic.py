272f9900c3836a29f5ea3ec8acff640ca97064f4
Buchanan Smith <bsmith19@bsmschool.org>
2017-12-15 11:31:30 -0800

    fix import

--- RoboPiLib/basic.py
+++ RoboPiLib/basic.py
@@ -1,5 +1,5 @@
-import RoboPiLib.RoboPiLib as RPL
-import RoboPiLib.setup
+from setup import RPL
+import post_to_web as PTW # see post_to_web.py for instructions
 
 sensor_pin = 17
 RPL.pinMode(sensor_pin,RPL.INPUT)
