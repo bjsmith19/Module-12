adc7a5b4c4557c18850682865afdabfd3e5c044f
Buchanan Smith <bsmith19@bsmschool.org>
2017-12-15 11:39:45 -0800

    fix import

--- Untitled.py
+++ Untitled.py
@@ -2,8 +2,8 @@ import RoboPiLib.RoboPiLib as RPL
 import RoboPiLib.setup
 
 sensor_pin = 16
-
+RPL.pinMode(sensor_pin,RPL.INPUT)
 
 while True:
-    sensorRead=RPL.pinMode(sensor_pin,RPL.INPUT)
+    sensorRead = digitalRead(sensor_pin])
     print sensorRead
