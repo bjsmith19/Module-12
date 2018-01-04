b12b9414e4ac3fa7f87870495c1b544995d00b10
bjsmith19 <33637189+bjsmith19@users.noreply.github.com>
2018-01-04 11:51:54 -0800

    Update Sensors.py

--- Sensors.py
+++ Sensors.py
@@ -2,8 +2,8 @@ from setup import RPL
 import post_to_web as PTW
 
 sensor_pin = 16
-RPL.pinMode(sensor_pin,RPL.INPUT)
+RPL.pinMode(16,RPL.INPUT)
 
 while True:
-  PTW.state['d1'] = RPL.digitalRead(sensor_pin)
+  PTW.state['d1'] = RPL.digitalRead(16)
   PTW.post()
