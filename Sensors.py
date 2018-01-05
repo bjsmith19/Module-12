13839a82f6a5555e6990948d0905845cd95c85e9
Buchanan Smith <bsmith19@bsmschool.org>
2018-01-05 11:25:59 -0800

    c

--- Sensors.py
+++ Sensors.py
@@ -1,9 +1,11 @@
-import RoboPiLib.RoboPiLib as RPL
+import RoboPiLib.RoboPiLib import RPL
 import RoboPiLib.setup
+import RoboPiLib.Post_to_web as PTW
 
-sensor_pin = 16
+sensor_pin ls = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
 while True:
   PTW.state['d1'] = RPL.digitalRead(sensor_pin)
-  PTW.post()
+
+PTW.post()
