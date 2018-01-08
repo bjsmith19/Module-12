6af12f772820c2de68e998af1d8fd37ffa345a91
bjsmith19 <33637189+bjsmith19@users.noreply.github.com>
2018-01-08 11:37:48 -0800

    Add files via upload

---
+++ Sensors2.py
@@ -0,0 +1,10 @@
+import setup as RPL
+from setup import RPL
+import post_to_web as PTW
+
+sensor_pin = 16
+RPL.pinMode(sensor_pin,RPL.INPUT)
+
+while True:
+  PTW.state['d1'] = RPL.digitalRead(sensor_pin)
+  PTW.post()
